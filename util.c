ab1b9f72284896139cd578afda7031d96510bdd4
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2020-10-22 15:07:41 +0530

    add support for machine-dependent headers

--- util.c
+++ util.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "util.h"
+
+#if defined(HAVE_STRING_H)
+#include <string.h>
+#elif defined(HAVE_STRINGS_H)
+#include <strings.h>
+#endif
+
 int util(int argc)
 {
 	printf("argc %d\n", argc);
